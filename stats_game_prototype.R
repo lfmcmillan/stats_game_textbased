c8ded613a31fe2283c48ae6f85037bba10edb96b
lfmcmillan <louise.mcmillan@gmail.com>
2021-12-24 21:17:10 +1300

    Extracted text blocks into stats_game_text_elements.R

--- stats_game_prototype.R
+++ stats_game_prototype.R
@@ -51,18 +51,7 @@ setup_user_progress <- function() {
 }
 
 intro <- function(user_name, progress, world) {
-    show_text(c("This is Stats Island. You and 100 other people crash-landed near
-               the island and swam to shore. You discovered that the island is
-               deserted, but there is a hut that has some scientific equipment
-               and historical records. It appears that this island was once a
-               research outpost.",
-               "A crate washes ashore containing some luggage, including a laptop
-               and a portable solar panel charger. You can use the hut to perform
-               scientific experiments, and analyse data on the laptop. The
-               existing data, and any new experiments, will help you work out
-               how to survive on the island.",
-               "None of the other survivors knows anything about statistics, but
-               they will help as much as they can."))
+    show_text(intro_text)
 
     readline(prompt="Please press [Enter] to continue")
     writeLines(cli::rule(line = 2))
@@ -95,18 +84,7 @@ run_level <- function(user_name) {
 
     switch(as.character(progress$level),
            "1" = {
-               show_text(c("A few hours after you find the hut, one of the survivors
-                     comes to find you. 'We've been exploring the island,' she
-                     says, 'and there are two streams, but we don't know which
-                     one is better to drink from. Are there any records that
-                     could help?'",
-                     "You look through the historical data in the hut, and find
-                     a set of data from the two streams: samples taken from
-                     each of the two streams once per month for a year. Each
-                     time, the researchers measured the mass of contaminants
-                     found in every 100ml of water from the stream. Analyse the
-                     results to see which stream is less contaminated."))
-
+               show_text(level_text[[1]])
                samples <- generate_samples(1, 2, world$streams)
                generated <- generate_sample_question_set(samples, "lowest")
                plot_stats_samples(samples)
@@ -114,31 +92,12 @@ run_level <- function(user_name) {
                update_progress(user_name, progress, world, correct_answer)
            },
            "2" = {
-               show_text(c("Somehow, the survivors all find places to sleep, in the
-                         few existing huts or in makeshift shelters. At least the
-                         nights don't seem to be too cold here. You start to
-                         wonder what the typical weather is like on this island.",
-                         "Digging through the old records, you find that the
-                         scientists who were here kept thorough records for years
-                         of the daily temperature, rainfall and highest wind
-                         speed. So you can look through them to see what the
-                         island's weather patterns are like."))
+               show_text(level_text[[2]])
                correct_answer <- show_weather_questions(world$weather)
                update_progress(user_name, progress, world, correct_answer)
            },
            "3" = {
-               show_text(c("Later in the day, another survivor comes to find you.
-                         We've got two different materials we can build shelter
-                         roofs from: palm leaves, or some kind of tree that looks
-                         like a variety of willow. Is there any data on which one
-                         is better for building?",
-                         "Looking through the hut datasets, you find that the
-                         scientists who were here called the willow-like tree
-                         'pacific etang', and they tested both it and palm leaves
-                         by building shelters out of them and then testing how much
-                         rain got in on each rainy day. That won't account for
-                         how well-built the shelters were, but it would be a good
-                         place to start."))
+               show_text(level_text[[3]])
                samples <- generate_samples(1, 2, world$shelter_materials)
                generated <- generate_boxplot_question_set(samples)
                plot_boxplot(samples, "Total rain leaked (mm)")
@@ -146,41 +105,10 @@ run_level <- function(user_name) {
                update_progress(user_name, progress, world, correct_answer)
            },
            "4" = {
-               show_text(c("A few days later, on the tenth day since the crash,
-                         the psychiatrist comes to find you.",
-                         "I want to evaluate how traumatised all of the survivors
-                         are, but it's very time consuming. I could use the
-                         survivors' self-assessments of how they are feeling,
-                         to decide who to prioritise, but I don't know how
-                         accurate their self-assessments are.",
-                         "Here are the self-assessments and my assessments for a
-                         subset of 30 survivors. They completed the
-                         self-assessments before I did my assessments. Each
-                         assessment is a score from 1 meaning 'Feeling very well'
-                         to 10 meaning 'Feeling very bad'. Would you please
-                         compare them, to see whether the self-assessments would
-                         be good enough for me to use for triage?"))
+               show_text(level_text[[4]])
                plot_scatterplot(world$trauma_assessments)
                correct_answer <- show_scatterplot_questions(world$trauma_assessments, linear=TRUE)
                update_progress(user_name, progress, world, correct_answer)
-           },
-           "5" = {
-               ### BAR CHARTS for 2 variables? Or 1 var and > 2 categories?
-           },
-           "6" = {
-               ### ESTIMATES VS. PARAMETERS [normal distribution]
-           },
-           "7" = {
-               ### MIXTURE OF PLOTS (different factors related to trauma):
-               # age
-               # number of flights taken/countries visited in the past
-               # height
-               # weight
-               # income
-               # education level
-           },
-           "8" = {
-               ### ESTIMATES VS. PARAMETERS [proportion] (rainy vs. dry days)
            },
            {
                writeLines("Sorry, that's the end of the game, you can't go any further yet.")
--- stats_game_text_elements.R
+++ stats_game_text_elements.R
@@ -1,4 +1,65 @@
 welcome_question <- "Welcome to Stats Island v1.0. What would you like to do?"
+intro_text <- c("This is Stats Island. You and 100 other people crash-landed near
+                the island and swam to shore. You discovered that the island is
+                deserted, but there is a hut that has some scientific equipment
+                and historical records. It appears that this island was once a
+                research outpost.",
+                "A crate washes ashore containing some luggage, including a laptop
+                and a portable solar panel charger. You can use the hut to perform
+                scientific experiments, and analyse data on the laptop. The
+                existing data, and any new experiments, will help you work out
+                how to survive on the island.",
+                "None of the other survivors knows anything about statistics, but
+                they will help as much as they can.")
+
 correct_answer_text <- "Yes! Correct answer."
 wrong_answer_text <- "No, wrong answer."
+
+level_text <- list("1"=c("A few hours after you find the hut, one of the survivors
+                         comes to find you. 'We've been exploring the island,' she
+                         says, 'and there are two streams, but we don't know which
+                         one is better to drink from. Are there any records that
+                         could help?'",
+                         "You look through the historical data in the hut, and find
+                         a set of data from the two streams: samples taken from
+                         each of the two streams once per month for a year. Each
+                         time, the researchers measured the mass of contaminants
+                         found in every 100ml of water from the stream. Analyse the
+                         results to see which stream is less contaminated."),
+                   "2"=c("Somehow, the survivors all find places to sleep, in the
+                         few existing huts or in makeshift shelters. At least the
+                         nights don't seem to be too cold here. You start to
+                         wonder what the typical weather is like on this island.",
+                         "Digging through the old records, you find that the
+                         scientists who were here kept thorough records for years
+                         of the daily temperature, rainfall and highest wind
+                         speed. So you can look through them to see what the
+                         island's weather patterns are like."),
+                   "3"=c("Later in the day, another survivor comes to find you.
+                         We've got two different materials we can build shelter
+                         roofs from: palm leaves, or some kind of tree that looks
+                         like a variety of willow. Is there any data on which one
+                         is better for building?",
+                         "Looking through the hut datasets, you find that the
+                         scientists who were here called the willow-like tree
+                         'pacific etang', and they tested both it and palm leaves
+                         by building shelters out of them and then testing how much
+                         rain got in on each rainy day. That won't account for
+                         how well-built the shelters were, but it would be a good
+                         place to start."),
+                   "4"=c("A few days later, on the tenth day since the crash,
+                         the psychiatrist comes to find you.",
+                         "I want to evaluate how traumatised all of the survivors
+                         are, but it's very time consuming. I could use the
+                         survivors' self-assessments of how they are feeling,
+                         to decide who to prioritise, but I don't know how
+                         accurate their self-assessments are.",
+                         "Here are the self-assessments and my assessments for a
+                         subset of 30 survivors. They completed the
+                         self-assessments before I did my assessments. Each
+                         assessment is a score from 1 meaning 'Feeling very well'
+                         to 10 meaning 'Feeling very bad'. Would you please
+                         compare them, to see whether the self-assessments would
+                         be good enough for me to use for triage?")
+)
 
